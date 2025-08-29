28101fdae2908cd58dae7c81e87d515ff2a969d4
Karan Samrat <karansamrat05039@gmail.com>
2025-08-29 15:05:18 +0530

    current chat added

--- BackEnd/route/userRoute.js
+++ BackEnd/route/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import isLogin from '../middleware/isLogin.js'
-import {getUserBySearch} from "../routeControlers/userRouteControler.js"
+import {getUserBySearch, currentChat} from "../routeControlers/userRouteControler.js"
 
 
 const router = express.Router()
 
 router.get('/search',isLogin,getUserBySearch)
+router.get('/currentchat', isLogin, currentChat)
 
 export default router
--- BackEnd/routeControlers/userRouteControler.js
+++ BackEnd/routeControlers/userRouteControler.js
@@ -1,3 +1,4 @@
+import conversation from "../Models/conversationModel.js"
 import User from "../Models/userModels.js"
 
 export const getUserBySearch = async (req, res) => {
@@ -21,6 +22,69 @@ export const getUserBySearch = async (req, res) => {
         
     } catch (error) {
         res.status(500).send({success:false,message: error} )
-        console.log('error in userRouteControler' ,error)
+        console.log('error in userRouteControler and getUserBySearch' ,error)
     }
 }
+
+// export const currentChat = async (req ,res) =>{
+//     try {
+//           const currentUserId = req.user._id
+//           const currentChaterrs = await conversation.find({
+//             participants: currentUserId
+//           }).sort({
+//             updatedAt: -1
+//           })
+//           if(!currentChaterrs || currentChaterrs.length === 0) return res.status(200).send([])
+
+//             const participantsId = currentChaterrs.reduce((ids,conversation)=>{
+//                 const otherParticipants = conversation.participants.filter(id => id.toString() !== currentUserId.toString());
+//                 return [...ids, otherParticipants]
+//             },[])
+//                console.log("Other Participants IDs:", participantsId);
+//             const otherParticipantsId = participantsId.filter(id=> id.toString() !== currentUserId.toString())
+//             console.log(otherParticipantsId)
+
+//             const user  = await User.find({_id:{$in:otherParticipantsId}}).select('-password').select('-email');
+
+//             const users = otherParticipantsId.map(id => user.find(user=> user._id.toString() === id.toString()))
+
+//             res.status(200).send(users)
+
+//     } catch (error) {
+//         res.status(500).send({success:false,message: error} )
+//         console.log('error in userRouteControler and currentChat' ,error)
+//     }
+// }
+
+export const currentChat = async (req, res) => {
+    try {
+        const currentUserId = req.user._id;
+
+        const currentChaterrs = await conversation.find({
+            participants: currentUserId
+        }).sort({ updatedAt: -1 });
+
+        if (!currentChaterrs || currentChaterrs.length === 0) {
+            return res.status(200).send([]);
+        }
+
+        // Collect all other participants (excluding current user)
+        const participantsId = currentChaterrs.reduce((ids, conversation) => {
+            const otherParticipants = conversation.participants.filter(
+                id => id.toString() !== currentUserId.toString()
+            );
+            return [...ids, ...otherParticipants]; // flatten
+        }, []);
+
+        console.log("Other Participants IDs:", participantsId);
+
+        const users = await User.find({ _id: { $in: participantsId } })
+            .select("-password -email");
+
+        res.status(200).send(users);
+
+    } catch (error) {
+        console.log("error in userRouteControler and currentChat", error);
+        res.status(500).send({ success: false, message: error.message });
+    }
+};
